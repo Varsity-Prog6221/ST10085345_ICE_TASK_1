ee0f12fc706f3582453b3e17e7033ebfc48c6c74
Liehan Els <ST10085345@vcconnect.edu.za>
2022-04-04 20:04:51 +0200

    FINAL

--- Take5groceries/Program.cs
+++ Take5groceries/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace Take5groceries
 {
@@ -10,22 +11,24 @@ namespace Take5groceries
             ConsoleKeyInfo cki;
             Item item = new Item();
             Cart cart = new Cart();
-            string welcomeMsg = " __________________________________________ \n" +
-                                "|       Welcome to Take 5 Groceries!       |\n" +
-                                "|==========================================|\n" +
-                                "|   Start   |  Press <Enter>               |\n" +
-                                "|==========================================|\n" +
-                                "|   Cancel  |  Press <Esc>                 |\n" +
-                                "|==========================================|\n" +
-                                "|   Exit    |  Press <F4>                  |\n" +
-                                "|__________________________________________|";
-            //creates an empty cart, displays start up message and waits for user input
+            //main menu
+            string welcomeMsg = "|==================================|\n" +
+                                "| * Welcome to Take 5 Groceries! * |\n" +
+                                "|==================================|\n" +
+                                "|    Start     |  Press <Enter>    |\n" +
+                                "|--------------|-------------------|\n" +
+                                "|    Cancel    |  Press <Esc>      |\n" +
+                                "|--------------|-------------------|\n" +
+                                "|    Exit      |  Press <F4>       |\n" +
+                                "|==================================|\n\n" +
+                                " Waiting for user input...";
+
+            //creates an empty cart, displays main menu and waits for user input
             cart.newCart();
             Console.WriteLine(welcomeMsg);
             cki = Console.ReadKey(true);
 
             //while-loop to continuously run program (main logic)
-
             while (checkExit(cki).Equals("Exit") == false)
             {
                 //declare input variables
@@ -35,40 +38,64 @@ namespace Take5groceries
                 int i = 0;
 
                 //while-loop to add 5 items to cart or exit on user demand
-
                 while (i < 5 && checkExit(cki).Equals("Enter") == true)
                 {
+                    string display;
+                    int fillChars = 43;
+                    string doubleLine = cart.addSpace(fillChars, "=");
                     //item name
-                    Console.WriteLine("Please enter item number {0}'s name : ", (i+1));
+                    display = "|" + doubleLine                            + "|\n" +
+                              "|   Please enter item number {0}'s name :     |\n" +
+                              "|" + doubleLine                            + "|\n";
+                    Console.WriteLine(display, (i + 1));
                     iName = Console.ReadLine();
 
                     //item price
-                    Console.WriteLine("Please enter the price of the " + iName + " : ");
+                    // 36 - base amount of characters to add to iName.Length
+                    fillChars = iName.Length + 36;
+                    doubleLine = cart.addSpace(fillChars, "=");
+                    display = "|" + doubleLine +                                "|\n" +
+                              "|   Please enter the price of the " + iName + "   |\n" +
+                              "|" + doubleLine +                                "|\n";
+                    Console.WriteLine(display);
                     //try-catch for string => double conversion
                     try
                     {
                         string priceInput = Console.ReadLine();
                         iPrice = Convert.ToDouble(priceInput);
-                        //adds item to cart
-                        item.newItem(iName, iPrice, i, cart);
-                        i++;
-                        if (i < 5)
+                        fillChars = iName.Length + iPrice.ToString().Length + 36;
+                        doubleLine = cart.addSpace(fillChars, "=");
+                        Console.WriteLine(  "|" + doubleLine +                              "|\n" +
+                                            "|  " + iName + " : R " + iPrice + cart.addSpace(29, " ") + "|\n" +
+                                            "|" + doubleLine +                              "|\n" +
+                                            "|  Press <Enter> to confirm item" + cart.addSpace(iName.Length + iPrice.ToString().Length + 5, " ") + "|\n" +
+                                            "|  Press <Spacebar> to redo item" + cart.addSpace(iName.Length + iPrice.ToString().Length + 5, " ") + "|\n" +
+                                            "|  Press <Esc> to cancel" + cart.addSpace(iName.Length + iPrice.ToString().Length + 13, " ") + "|\n" +
+                                            "|" + doubleLine +                              "|\n");
+                        cki = Console.ReadKey(true);
+                        if (cki.Key == ConsoleKey.Enter)
                         {
-                            Console.WriteLine("|=========================================================|\n" +
-                                              "|    Press <Enter> to add next item or <Esc> to cancel    |\n" +
-                                              "|=========================================================|\n");
-                            cki = Console.ReadKey(true);
+                            //adds item to cart
+                            item.newItem(iName, iPrice, i, cart);
+                            i++;
+                        } else if (cki.Key == ConsoleKey.Spacebar)
+                        {
+                            Console.WriteLine("Re-doing last item");
+                            iPrice = 0.0;
+                        } else if (cki.Key == ConsoleKey.Escape)
+                        {
+                            Console.WriteLine("Do I need to do anything here ?");
                         }
                     }
                     catch (Exception exE)
                     {
                         Console.WriteLine("|=====================================================|\n" +
-                                          "|                        Error :                      |\n" +
-                                          "|=====================================================|\n" +
-                                          exE + "\n" +
-                                          "|=====================================================|\n" +
-                                          "|   Press enter to re-add last item or Esc to cancel  |\n" +
-                                          "|=====================================================|\n");
+                                          "|      Invalid Input Error :  See details below       |\n" +
+                                          "|=====================================================|\n\n" +
+                                          exE + "\n\n" +
+                                          "|=========================================================|\n" +
+                                          "|   Press <Enter> to re-add last item or <Esc> to cancel  |\n" +
+                                          "|=========================================================|\n\n");
                         iPrice = 0.0;
                         cki = Console.ReadKey(true);
                     }
@@ -78,9 +105,10 @@ namespace Take5groceries
                 {
                     //display cart
                     Console.WriteLine(cart.ToString());
-                    Console.WriteLine("Press enter to continue...");
+                    Console.WriteLine("Press enter to return to main menu...");
                     Console.ReadLine();
                 }
+                Console.Clear();
                 Console.WriteLine(welcomeMsg);
                 cki = Console.ReadKey(true);
             }
@@ -95,9 +123,17 @@ namespace Take5groceries
             {
                 return "Cancel";
             }
-            else
+            else if (cki.Key == ConsoleKey.Enter)
             {
                 return "Enter";
+            }
+            else if (cki.Key == ConsoleKey.Spacebar)
+            {
+                return "Enter";
+            }
+            else
+            {
+                return "Invalid option selected";
             }
         }
     }
@@ -110,11 +146,11 @@ namespace Take5groceries
     }
     public class Cart
     {
-        string[] itemNames = new string[5];
-        double[] itemPrices = new double[5];
-        double total;
-        double vatRate;
-        double totalVat;
+        private string[] itemNames = new string[5];
+        private double[] itemPrices = new double[5];
+        private double total;
+        private double vatRate;
+        private double totalVat;
         public void newCart()
         {
             totalVat = 0;
@@ -132,7 +168,8 @@ namespace Take5groceries
             itemPrices[index] = itemPrice;
             total = total + itemPrice;
             totalVat = total * vatRate;
-        } public string addSpace(int numOfSpaces, string fill)
+        }
+        public string addSpace(int numOfSpaces, string fill)
         {
             string spaces = "";
             for (int i = 0; i < numOfSpaces; i++)
@@ -143,9 +180,10 @@ namespace Take5groceries
         }
         public override string ToString()
         {
-            string fill =          "|------------------------------------------------|\n";//50 chars
-            string ends =          "|================================================|\n";
-            string output = ends + "|     Thank you, here are your cart details      |\n" + fill;
+            //receipt code formatted to resemble output
+            string fill =                   "|------------------------------------------------|\n";
+            string ends =                   "|================================================|\n";
+            string output = ends +          "|     Thank you, here are your cart details      |\n" + fill;
             string column1, column2, line = "";
             int totalLineChars = fill.Length - 2;
             int col2Chars = 17;
@@ -155,22 +193,23 @@ namespace Take5groceries
                 column1 = "|  " + itemNames[i];
                 column2 = itemPrices[i] + "  |";
 
-                usedChars = column2.Length-2;
+                usedChars = column2.Length - 2;
 
-                column2 = "|  R" + addSpace((col2Chars-usedChars),".") + column2;
+                column2 = "|  R" + addSpace((col2Chars - usedChars), ".") + column2;
 
                 usedChars = column1.Length + column2.Length;
                 //spacing maths
                 int middle = 0;
                 if (usedChars > totalLineChars)
                 {
-                    column1 = column1 + addSpace(totalLineChars-column1.Length," ") + "|\n|";
+                    column1 = column1 + addSpace(totalLineChars - column1.Length, " ") + "|\n|";
                     middle = totalLineChars - column2.Length;
-                } else
+                }
+                else
                 {
                     middle = totalLineChars - usedChars + 1;
                 }
-                line = column1 + addSpace(middle," ") + column2 + "\n";
+                line = column1 + addSpace(middle, " ") + column2 + "\n";
                 output = output + line;
             }
             totalVat = Math.Round((totalVat * 100.0)) / 100.0;
